0c4896dba0723cc59fe6fa9bf2b5dc7843113146
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-05-28 12:26:19 -0300

    feat: set ship length with value passed to constructor

--- src/ship.js
+++ src/ship.js
@@ -1,10 +1,31 @@
 class Ship{
-    constructor(length){
-        this.length = length || 0
+    constructor(type){
+        this.length = this.shipLength(type)
+        // this.class = this.shipClass(type)
         this.hits = 0
         this.sunk = false
     }
 
+
+    // determine length of ship based on it's type
+    shipLength(type){
+        if (type === 1){
+            return this.length = 5
+            this.class = 'Carrier';
+        } else if (type === 2){
+            return this.length = 4   
+            this.class = 'Battleship';
+        } else if (type === 3){
+            return this.length = 3
+            this.class = 'Cruiser';
+        } else if (type === 4){
+            return this.length = 3
+            this.class = 'Submarine';
+        } else if (type === 5){
+            return this.length = 2
+            this.class = 'Destroyer';
+        }
+    }
     // increases the number of ‘hits’ in your ship.
     hit(){
         this.hits+=1
