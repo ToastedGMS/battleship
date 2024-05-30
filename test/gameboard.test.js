e64dffc8766dfc66119189c5a87a3e74387a5b01
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-05-30 14:16:45 -0300

    test: test 'receiveAttack' function

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -9,3 +9,14 @@ test('correctly places ship in provided coordinates', () => {
     expect(gameboard.a[2]).toBe(3)
     expect(gameboard.a[1]).toBe(ship1)
 })
+
+test('correctly determines if given coordinates hit a ship or not', () => {
+    const gameboard = new Gameboard
+    const ship1 = new Ship(5)
+    gameboard.placeShip(ship1, ['a1', 'b1'])
+    gameboard.receiveAttack('a1')
+    gameboard.receiveAttack('a2')
+
+    expect(ship1.hits).not.toBe(0)
+    expect(gameboard.a[2]).toBe('miss')
+})
