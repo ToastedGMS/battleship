38bdd78143eaf22e88169e2265bca433e9d438ef
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-06-03 12:23:25 -0300

    feat: render player gameboards using information from player objects

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const Player = require("./player");
+const Ship = require("./ship");
+
+const player1 = new Player
+const plr1Ship1 = new Ship(5)
+player1.gameboard.placeShip(plr1Ship1, ['c3','d3'])
+
+const player2 = new Player
+const plr2Ship1 = new Ship(5)
+player2.gameboard.placeShip(plr2Ship1, ['h8','h9'])
+
+const masterbtn = document.querySelector('#masterbtn')
+const wrapper = document.querySelector('.wrapper')
+
+function createGrid(board) {
+    const container = document.createElement('div');
+    container.className = 'grid-container';
+
+    const rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+
+    rows.forEach(row => {
+        board[row].forEach(cell => {
+            const cellElement = document.createElement('div');
+            cellElement.className = 'grid-item';
+            if ( !isNaN(cell) == false){
+                cellElement.textContent = cell.class;
+            } else {
+                cellElement.textContent = cell;
+            }
+            container.appendChild(cellElement);
+        });
+    });
+
+    wrapper.appendChild(container);
+}
+
+masterbtn.addEventListener('click', () => {createGrid(player1.gameboard), createGrid(player2.gameboard)})
+
